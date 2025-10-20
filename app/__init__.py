ecec300cc881b15496fafa3139c74e9e00e88f6c
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-10-20 01:48:22 -0300

    fix: circular import due to injector.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask, redirect
-from flask_injector import FlaskInjector
 from flask_smorest import Api
 
 
 def create_app() -> Flask:
+    from flask_injector import FlaskInjector
     app = Flask(__name__)
 
     from app.config import Config
@@ -11,14 +11,14 @@ def create_app() -> Flask:
 
     api = Api(app)
 
-    from app import injector
-    FlaskInjector(app, modules=[injector.SqliteModule()])
-
     from app import database
     database.create_all()
 
     from app import routes
     api.register_blueprint(routes.auth)
+
+    from app import injector
+    FlaskInjector(app=app, modules=[injector.SqliteModule()])
 
     app.add_url_rule('/', endpoint='index', view_func=lambda: redirect('/api/v1/docs'))
 
--- app/injector.py
+++ app/injector.py
@@ -1,18 +1,14 @@
-from typing import TYPE_CHECKING
+import sqlite3
 
+from flask import Flask
 from flask_injector import request
 
 from injector import Module, provider
 
-if TYPE_CHECKING:
-    import sqlite3
-
-    from flask import Flask
-
 
 class SqliteModule(Module):
-    @request
     @provider
+    @request
     def provide_db_connection(self, app: Flask) -> sqlite3.Connection:
         from app.database import get_connection
 
